fb19754ea3fbba57ddb658d37ffa7b0203a2c795
Abhishek Anand <anandabhishek3607@gmail.com>
2023-04-13 15:25:07 +0530

    Matching number of workers to cpu

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
 const express = require('express');
+const cluster = require('cluster');
+const os = require('os');
 
 const app = express();
 
 function delay(duration) {
     const startTime = Date.now();
     while (Date.now() - startTime < duration) {
-        // event loop is blocked
+        //event loop is blocked...
     }
 }
 
 app.get('/', (req, res) => {
     // JSON.stringify({}) => "{}"
-    // JSON.parse("{}")=> {}
-    res.send('Performance example');
+    // JSON.parse("{}") => {}
+    // [5,1,2,3,4].sort()
+    res.send(`Performance example: ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
-    // delay the response
-    delay(9200);
-    res.send('Ding ding dingas!');
-})
+    delay(9000);
+    res.send(`Ding ding ding! ${process.pid}`);
+});
 
-app.listen(3000);
+console.log('Running server.js...');
+if (cluster.isMaster) {
+    console.log('Master has been started...');
+    const NUM_WORKERS = os.cpus().length;
+    for (let i = 0; i < NUM_WORKERS; i++) {
+        cluster.fork();
+    }
+} else {
+    console.log('Worker process started.');
+    app.listen(3000);
+}
